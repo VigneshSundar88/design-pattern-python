7a1b1c3d7c2dfedbc58bc4731e87d0fbc6dea926
VigneshSundar88 <mailtovignesh88@gmail.com>
2023-07-25 07:33:23 -0600

    Delete unified_framework directory

--- unified_framework/io_package/job_module.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-
-class job_module(ABC):
-    def getSpark():
-        spark = SparkSession.Builder().appName("unified_framework").getOrCreate()
-        return spark
